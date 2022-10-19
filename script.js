cc70a5108f2dda113c5526f918d746b46eeebcb8
Maggie-6 <maggierani@yahoo.com>
2022-10-19 12:57:48 -0400

    /home/project/vftvk-Simple-Interest-Calculator

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function compute() {   //computes principal, intererst and rate
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+  
+    if (principal <= 0) {
+      alert("Enter Positive Number");// shows alert for negative numbers
+      principal.focus();
+      return false;
+    }
+  
+    result = document.getElementById("result"); //displays results in html span
+    result.innerHTML =
+      "If you deposit " +
+      principal +
+      "<br>at and interest rate of " +
+      rate +
+      "%<br>You will receive an amount of " +
+      interest +
+      ",<br>in the year " +
+      year +
+      "<br>";
+  }
+  
+  function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+  }
+  
